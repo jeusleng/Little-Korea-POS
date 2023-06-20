1f964734176f251b0664e3b95078249a1a8b6600
JawenDev <janwayne.cunanan03@gmail.com>
2023-06-20 09:46:42 +0800

    added dashboard

--- SplashScreen/AdminDashboard.Designer.cs
+++ SplashScreen/AdminDashboard.Designer.cs
@@ -29,21 +29,211 @@ namespace SplashScreenLadera
         /// </summary>
         private void InitializeComponent()
         {
+            this.cashierChart = new LiveCharts.WinForms.PieChart();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.countTransLabel = new Guna.UI2.WinForms.Guna2TextBox();
+            this.countItemsLabel = new Guna.UI2.WinForms.Guna2TextBox();
+            this.countSoldLabel = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2TextBox5 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2TextBox6 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2TextBox7 = new Guna.UI2.WinForms.Guna2TextBox();
             this.SuspendLayout();
             // 
-            // Dashboard
+            // cashierChart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.cashierChart.Location = new System.Drawing.Point(12, 300);
+            this.cashierChart.Name = "cashierChart";
+            this.cashierChart.Size = new System.Drawing.Size(308, 189);
+            this.cashierChart.TabIndex = 1;
+            this.cashierChart.Text = "cashierChart";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderThickness = 0;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "Cashier(Active/Inactive) Chart";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(55, 258);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(200, 36);
+            this.guna2TextBox1.TabIndex = 2;
+            this.guna2TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // countTransLabel
+            // 
+            this.countTransLabel.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.countTransLabel.DefaultText = "0";
+            this.countTransLabel.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.countTransLabel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.countTransLabel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countTransLabel.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countTransLabel.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countTransLabel.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.countTransLabel.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countTransLabel.Location = new System.Drawing.Point(24, 26);
+            this.countTransLabel.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.countTransLabel.Name = "countTransLabel";
+            this.countTransLabel.PasswordChar = '\0';
+            this.countTransLabel.PlaceholderText = "";
+            this.countTransLabel.SelectedText = "";
+            this.countTransLabel.Size = new System.Drawing.Size(172, 85);
+            this.countTransLabel.TabIndex = 3;
+            this.countTransLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // countItemsLabel
+            // 
+            this.countItemsLabel.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.countItemsLabel.DefaultText = "0";
+            this.countItemsLabel.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.countItemsLabel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.countItemsLabel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countItemsLabel.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countItemsLabel.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countItemsLabel.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.countItemsLabel.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countItemsLabel.Location = new System.Drawing.Point(229, 26);
+            this.countItemsLabel.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.countItemsLabel.Name = "countItemsLabel";
+            this.countItemsLabel.PasswordChar = '\0';
+            this.countItemsLabel.PlaceholderText = "";
+            this.countItemsLabel.SelectedText = "";
+            this.countItemsLabel.Size = new System.Drawing.Size(172, 85);
+            this.countItemsLabel.TabIndex = 4;
+            this.countItemsLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // countSoldLabel
+            // 
+            this.countSoldLabel.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.countSoldLabel.DefaultText = "0";
+            this.countSoldLabel.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.countSoldLabel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.countSoldLabel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countSoldLabel.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.countSoldLabel.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countSoldLabel.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.countSoldLabel.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.countSoldLabel.Location = new System.Drawing.Point(441, 26);
+            this.countSoldLabel.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.countSoldLabel.Name = "countSoldLabel";
+            this.countSoldLabel.PasswordChar = '\0';
+            this.countSoldLabel.PlaceholderText = "";
+            this.countSoldLabel.SelectedText = "";
+            this.countSoldLabel.Size = new System.Drawing.Size(172, 85);
+            this.countSoldLabel.TabIndex = 5;
+            this.countSoldLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // guna2TextBox5
+            // 
+            this.guna2TextBox5.BorderThickness = 0;
+            this.guna2TextBox5.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox5.DefaultText = "Total Transactions";
+            this.guna2TextBox5.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox5.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox5.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2TextBox5.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox5.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox5.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox5.Location = new System.Drawing.Point(24, 109);
+            this.guna2TextBox5.Name = "guna2TextBox5";
+            this.guna2TextBox5.PasswordChar = '\0';
+            this.guna2TextBox5.PlaceholderText = "";
+            this.guna2TextBox5.SelectedText = "";
+            this.guna2TextBox5.Size = new System.Drawing.Size(172, 36);
+            this.guna2TextBox5.TabIndex = 6;
+            this.guna2TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // guna2TextBox6
+            // 
+            this.guna2TextBox6.BorderThickness = 0;
+            this.guna2TextBox6.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox6.DefaultText = "Total Items";
+            this.guna2TextBox6.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox6.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox6.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox6.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox6.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2TextBox6.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox6.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox6.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox6.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox6.Location = new System.Drawing.Point(229, 109);
+            this.guna2TextBox6.Name = "guna2TextBox6";
+            this.guna2TextBox6.PasswordChar = '\0';
+            this.guna2TextBox6.PlaceholderText = "";
+            this.guna2TextBox6.SelectedText = "";
+            this.guna2TextBox6.Size = new System.Drawing.Size(172, 36);
+            this.guna2TextBox6.TabIndex = 7;
+            this.guna2TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // guna2TextBox7
+            // 
+            this.guna2TextBox7.BorderThickness = 0;
+            this.guna2TextBox7.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox7.DefaultText = "Total Items Sold";
+            this.guna2TextBox7.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox7.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox7.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox7.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox7.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2TextBox7.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox7.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox7.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox7.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox7.Location = new System.Drawing.Point(441, 109);
+            this.guna2TextBox7.Name = "guna2TextBox7";
+            this.guna2TextBox7.PasswordChar = '\0';
+            this.guna2TextBox7.PlaceholderText = "";
+            this.guna2TextBox7.SelectedText = "";
+            this.guna2TextBox7.Size = new System.Drawing.Size(172, 36);
+            this.guna2TextBox7.TabIndex = 8;
+            this.guna2TextBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(853, 629);
+            this.ClientSize = new System.Drawing.Size(640, 511);
+            this.Controls.Add(this.guna2TextBox7);
+            this.Controls.Add(this.guna2TextBox6);
+            this.Controls.Add(this.guna2TextBox5);
+            this.Controls.Add(this.countSoldLabel);
+            this.Controls.Add(this.countItemsLabel);
+            this.Controls.Add(this.countTransLabel);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.cashierChart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Dashboard";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "AdminDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.AdminDashboard_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private LiveCharts.WinForms.PieChart cashierChart;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox countTransLabel;
+        private Guna.UI2.WinForms.Guna2TextBox countItemsLabel;
+        private Guna.UI2.WinForms.Guna2TextBox countSoldLabel;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox5;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox6;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox7;
     }
 }
--- SplashScreen/AdminDashboard.cs
+++ SplashScreen/AdminDashboard.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace SplashScreenLadera
 {
@@ -16,6 +20,55 @@ namespace SplashScreenLadera
         public AdminDashboard()
         {
             InitializeComponent();
+        }
+
+        Connection dbCon = new Connection();
+
+        private void AdminDashboard_Load(object sender, EventArgs e)
+        {
+            populate();
+        }
+
+        void populate()
+        {
+            dbCon.openConnection();
+            string query1 = "select count(*) from usersTable where status = 'Active'";
+            SqlCommand cmd1 = new SqlCommand(query1, dbCon.getConnection());
+            int countActive = Convert.ToInt32(cmd1.ExecuteScalar());
+
+            string query2 = "select count(*) from usersTable where status = 'Inactive'";
+            SqlCommand cmd2 = new SqlCommand(query2, dbCon.getConnection());
+            int countInactive = Convert.ToInt32(cmd2.ExecuteScalar());
+
+            string query3 = "select count(*) from transactionsTable";
+            SqlCommand cmd3 = new SqlCommand(query3, dbCon.getConnection());
+            countTransLabel.Text = cmd3.ExecuteScalar().ToString();
+
+            string query4 = "select count(*) from productsTable";
+            SqlCommand cmd4 = new SqlCommand(query4, dbCon.getConnection());
+            countItemsLabel.Text = cmd4.ExecuteScalar().ToString();
+
+            string query5 = "select count(*) from transactionDetailsTable";
+            SqlCommand cmd5 = new SqlCommand(query5, dbCon.getConnection());
+            countSoldLabel.Text = cmd5.ExecuteScalar().ToString();
+            dbCon.closeConnection();
+
+            ChartValues<int> activeValues = new ChartValues<int> { countActive };
+            ChartValues<int> inactiveValues = new ChartValues<int> { countInactive };
+
+            cashierChart.Series.Clear();
+            cashierChart.Series.Add(new PieSeries
+            {
+                Title = "Active",
+                Values = activeValues
+            });
+            cashierChart.Series.Add(new PieSeries
+            {
+                Title = "Inactive",
+                Values = inactiveValues
+            });
+                
+           
         }
     }
 }
